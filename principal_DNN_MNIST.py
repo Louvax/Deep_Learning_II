7be0306db24a229a6300fc52a51ff312c1753566
Romain Louvet <romain.louvet@telecom-paris.fr>
2023-03-07 12:08:56 +0100

    End of retropropag

--- principal_DNN_MNIST.py
+++ principal_DNN_MNIST.py
@@ -28,14 +28,54 @@ class DNN:
         output.append(self.calcul_softmax(X_copy))
 
         return output
-    
+
     def retropropagation(self, X, y, epochs, batch_size, lr):
         for epoch in range(epochs):
-            X_copy = X.copy()
-            np.random.shuffle(X_copy)
+            X_copy, y_copy = X.copy(), y.copy()
+            assert len(X_copy) == len(y_copy)
+            p = np.random.permutation(len(X_copy))
+            X_copy, y_copy = X_copy[p], y_copy[p]
             for batch in range(0, len(X_copy), batch_size):
-                X_batch = X_copy[batch:min(batch+batch_size, len(X_copy))]
+                X_batch = X_copy[batch : min(batch + batch_size, len(X_copy))]
+                y_batch = y_copy[batch : min(batch + batch_size, len(y_copy))]
                 tb = len(X_batch)
+                z = self.entree_sortie_reseau(X_batch)
 
-                
+                dz1 = z[-1] - y_batch
+                grad_b = (1 / tb) * np.sum(dz1)
+                grad_w = (1 / tb) * (z[-2].T @ dz1)
+
+                self.RBM.W -= lr * grad_w
+                self.RBM.b -= lr * grad_b
+
+                dz2 = z[-2] * (1 - z[-2])
+                dz1 = (dz2 @ self.RBM.W.T) * dz2
+
+                grad_w = z[-3].T @ dz1 / tb
+                grad_b = np.sum(dz1) / tb
+
+                self.DBN.Dbn[-1].W -= lr * grad_w
+                self.DBN.Dbn[-1].b -= lr * grad_b
+
+                for layer in range(len(self.DBN) - 3, -1, -1):
+                    if layer == 0:
+                        data = X_batch
+                    else:
+                        data = z[layer - 1]
+
+                    dz2 = z[layer] * (1 - z[layer])
+                    dz1 = (dz2 @ self.DBN.Dbn[layer+1].W.T)*dz2
+
+                    grad_w = z[layer-1].T @ dz1 / tb
+                    grad_b = np.sum(dz1) / tb
+
+                    self.DBN.Dbn[layer].W -= lr * grad_w
+                    self.DBN.Dbn[layer].b -= lr * grad_b
+
+                z = self.entree_sortie_reseau(X_batch)
+
+            crossentropy = -np.mean(np.log10(z)[y_batch==1])
+
+            print(f'Epoch : {epoch}, Loss : {crossentropy}')
+            
 
